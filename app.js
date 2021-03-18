aca38ec2d4acc29061a727700687e02d2ce37fe8
RossScorgie <1905799@rgu.ac.uk>
2021-03-18 11:46:45 +0000

    Made templates for header and footer

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.send('working');
+    res.render('index');
 });
 
 //Server start
