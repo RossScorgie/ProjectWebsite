df586063b7f8960041f49d409656ca31402adeb4
RossScorgie <1905799@rgu.ac.uk>
2021-03-22 09:36:46 +0000

    Adding packages express messages, body parser and connect flash Adjusted header and footer layouts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require ('path');
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+var session = require('express-session')
 
 //db connect
 mongoose.connect('mongodb+srv://Admin:Admin@firstcluster.hqarp.mongodb.net/projectsite?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -19,6 +21,25 @@ app.set('view engine', 'ejs');
 
 //Public folder setup
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser for urlencoded and json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Express session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+//Express messages
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 //Setting Routes
 var pages = require('.routes/pages.js');
